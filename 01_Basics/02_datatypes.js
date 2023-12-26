bb8cfb745155f199cad3372e6ae80151299f0ab6
Kin Learns <109691311+kinlearn@users.noreply.github.com>
2023-12-26 12:30:33 +0000

    notes taken

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,17 @@
+'use strict'; // trear all js code as newer version of js
+
+console.log(2 
+    + 4) // please avoid bad readablility practices
+
+// www.tc39.es <-- original documentation of javascript
+
+let a = 'string'; // string
+a = 12 // Number
+a = true // boolean
+a = null // null (type object)
+let b; // undefined
+
+// There are two more premitive datatypes. Symbol and bigInt
+
+console.log(typeof a)
+
