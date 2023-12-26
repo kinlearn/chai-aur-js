347ba78fe15ae1a10c28a6b20e2a924c20eecd24
Kin Learns <109691311+kinlearn@users.noreply.github.com>
2023-12-26 14:57:02 +0000

    conversion of data types

--- 01_Basics/03_conversionOfDatatypes.js
+++ 01_Basics/03_conversionOfDatatypes.js
@@ -1,2 +1,18 @@
-'use strict';
+// 'use strict';
 
+let score = '22';
+
+// console.info(typeof score);
+
+let valueInNumber = Number(score); 
+console.log(typeof valueInNumber);
+
+
+score = null;
+// console.log(score); // null
+
+// Truthy and falsy values : 0, "", null, undefined, false become false all other become true
+
+let a = 0
+const truthy = a ? `truthy` : `falsy`;
+console.log(truthy);
